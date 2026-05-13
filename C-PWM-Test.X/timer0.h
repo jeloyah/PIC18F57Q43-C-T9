f3be48e883057fa6da2d4efd30339c4104c7a3cf
jeloyah <jeloyah@gmail.com>
2026-05-13 12:39:00 -0700

    Change TIMER0_Delay_ms parameter type to unsigned short

--- C-PWM-Test.X/timer0.h
+++ C-PWM-Test.X/timer0.h
@@ -64,7 +64,7 @@
 void TIMER0_Initialize(void);
 void TIMER0_Start(void);
 void TIMER0_Stop(void);
-void TIMER0_Delay_ms(uint16_t Time);
+void TIMER0_Delay_ms(unsigned short Time);
 
 #ifdef	__cplusplus
 extern "C" {
